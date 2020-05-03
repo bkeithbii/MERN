3e3657ced187750cbb311399fa825b80f75b889e
Brian Brown II <bkeithbii@gmail.com>
2020-05-03 16:52:20 -0400

    Routes + Characters added

--- mern/src/components/App/App.js
+++ mern/src/components/App/App.js
@@ -7,6 +7,13 @@ import Character from "../Characters/Character";
 import Heroes from "../Characters/Heroes";
 import Home from "../Home/Home";
 import Villains from "../Characters/Villains";
+import Males from "../Characters/Males";
+import Females from "../Characters/Females";
+import Humans from "../Characters/Humans";
+import Mutants from "../Characters/Mutants";
+import Aliens from "../Characters/Aliens";
+import Marvel from "../Characters/Marvel";
+import DC from "../Characters/DC";
 
 class App extends Component {
   render() {
@@ -20,13 +27,13 @@ class App extends Component {
           <Route path="/character" component={Character} />
           <Route path="/heroes" component={Heroes} />
           <Route path="/villains" component={Villains} />
-          {/* <Route path="/males" component={Males} />
+          <Route path="/males" component={Males} />
           <Route path="/females" component={Females} />
           <Route path="/humans" component={Humans} />
           <Route path="/mutants" component={Mutants} />
           <Route path="/aliens" component={Aliens} />
           <Route path="/marvelcomics" component={Marvel} />
-          <Route path="/dccomics" component={DC} /> */}
+          <Route path="/dccomics" component={DC} />
         </main>
       </div>
     );
---
+++ mern/src/components/Characters/Humans.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "./Character.css";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import Powers from "./Powers";
+import humansClient from "../Clients/humansClient";
+
+class Humans extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      powers: [],
+      characters: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const client = new humansClient(); // Create new instance of client
+      const data = await client.fetchCharacters(); // Fetch characters from API returns resolved json
+      this.setState({ data }); // Update state with new data
+      this.setState({ characters: data.map((item) => item) });
+      this.setState({
+        powers: this.state.characters.map((character) => character.Powers),
+      });
+
+      console.log(JSON.stringify(this.state.powers, null, 2));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    let characters = this.state.characters.map((character) => {
+      return (
+        <div className="character" key={character.id}>
+          <Card
+            key={character.id}
+            bg="dark"
+            text="white"
+            style={{ width: "18rem" }}
+            className="text-center"
+          >
+            <Card.Header>{character.Publisher}</Card.Header>
+            <Card.Body>
+              <Card.Title>{character.Name}</Card.Title>
+              <Card.Text>Status: {character.Alignment}</Card.Text>
+              <Card.Text>Race: {character.Race}</Card.Text>
+              <Card.Text>Gender: {character.Gender}</Card.Text>
+            </Card.Body>
+
+            <Accordion key={character.id}>
+              <Card className="accordion">
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                  View Powers
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body className="powers">
+                    {" "}
+                    <Powers powers={character.Powers}></Powers>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Card>
+          <br />
+        </div>
+      );
+    });
+    return <div className="characterContainer">{characters}</div>;
+  }
+}
+
+export default Humans;
